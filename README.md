81cb31ba8c506615650d4b8cfddac4eb03f79bbd
Kwok <AndyKuo@yeah.net>
2022-08-07 12:45:00 +0800

    Add HttpDelete Agent .
    1.update src\main\java\org\devops\servlet\MainServlet.java .

--- README.md
+++ README.md
@@ -1,8 +1,8 @@
 # nat-agent-server
 ------
-HttpGet、HttpPost Agent .
+HttpGet、HttpPost、HttpDelete Agent .
 
-1. 设置安全访问IP白名单，系统`system.properties` 中的 `ip_white_list`值。
-2. 获取代理路径：GET  IP:port/settings。 设置代理路径：POST IP:port/settings。
-3. 可代理 GET、POST请求。
-
+1.设置安全访问IP白名单，系统`system.properties` 中的 `ip_white_list`值。
+2.获取代理路径：GET  IP:port/settings。
+  设置代理路径：POST IP:port/settings。
+3.可代理 GET、POST、DELETE 请求。
--- src/main/java/org/devops/servlet/MainServlet.java
+++ src/main/java/org/devops/servlet/MainServlet.java
@@ -12,7 +12,9 @@ import javax.servlet.http.HttpServletResponse;
 import org.devops.util.NativeCache;
 import org.nutz.http.Header;
 import org.nutz.http.Http;
+import org.nutz.http.Request;
 import org.nutz.http.Response;
+import org.nutz.http.Sender;
 import org.nutz.lang.Streams;
 import org.nutz.lang.Strings;
 
@@ -47,4 +49,19 @@ public class MainServlet extends HttpServlet {
 
     }
 
+    @Override
+    protected void doDelete(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.setContentType("text/plain; charset=UTF-8");
+        PrintWriter writer = resp.getWriter();
+        String uri = req.getRequestURI();
+        String urlParam = Strings.isBlank(req.getQueryString()) ? "" : "?" + req.getQueryString();
+
+        Request request = Request.create("http://" + NativeCache.agentHost + uri + urlParam, Request.METHOD.DELETE);
+        Sender sender = Sender.create(request, 60000);
+        Response Response = sender.send();
+        resp.setContentType(Response.getHeader().get("Content-Type", "text/plain; charset=UTF-8"));
+        writer.write(Response.getContent());
+    }
+    
+    
 }
